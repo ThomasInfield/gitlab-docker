b9db1a65fde1a864f629a22a322e8c110e88bd94
Thomas <thomas@wapnet.nl>
2025-08-29 00:48:48 +0200

    Enhance GitLab Docker setup with resource limits and SSL configuration for runners

--- register-runner.sh
+++ register-runner.sh
@@ -3,7 +3,7 @@ set -e
 
 # Fill in these variables:
 GITLAB_URL="https://gitlab.example.com/"
-REGISTRATION_TOKEN="glrt-GuGZ2ePA9E2TEzSSKUBnoW86MQp0OjEKdToxCw.01.121qv413q" # Get this from GitLab UI (Admin > Runners)
+REGISTRATION_TOKEN="glrt-ynF3FCBHoPQG1xyz3o-F0W86MQp0OjEKdToxCw.01.121ycc0rz" # Get this from GitLab UI (Admin > Runners)
 
 # Use the Docker network for container communication
 docker run --rm -it \
@@ -18,5 +18,7 @@ docker run --rm -it \
     --token "$REGISTRATION_TOKEN" \
     --name "docker-ansible-runner" \
     --executor "docker" \
+    --tls-ca-file "/etc/gitlab/ssl/gitlab.example.com.crt" \
+    --docker-volumes "/etc/gitlab/ssl:/etc/gitlab/ssl:ro" \
     --docker-image "quay.io/ansible/ansible-runner:latest" \
     --tls-ca-file "/etc/gitlab/ssl/gitlab.example.com.crt"
