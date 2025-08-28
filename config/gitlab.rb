ac03d6fef5e44703e9dcf239897c33b924233a3b
Thomas <thomas@wapnet.nl>
2025-08-28 22:32:03 +0200

    Update GitLab configuration to use HTTPS for external URLs and enable secure NGINX settings

--- config/gitlab.rb
+++ config/gitlab.rb
@@ -1,6 +1,6 @@
 # gitlab-docker/config/gitlab.rb
 
-external_url 'http://gitlab.example.com'
+external_url 'https://gitlab.example.com'
 
 gitlab_rails['gitlab_email_enabled'] = true
 gitlab_rails['gitlab_email_from'] = 'noreply@gitlab.example.com'
@@ -18,8 +18,8 @@ gitlab_rails['smtp_enable_starttls_auto'] = true
 
 gitlab_rails['gitlab_shell_ssh_port'] = 22
 
-nginx['listen_port'] = 80
-nginx['listen_https'] = false
+nginx['listen_port'] = 443
+nginx['listen_https'] = true
 
 letsencrypt['enable'] = false
 
@@ -31,8 +31,9 @@ registry['enable'] = true
 registry['host'] = 'registry.example.com'
 registry['port'] = 5000
 registry['path'] = '/registry'
-registry['api_url'] = 'http://registry.example.com:5000'
+registry['api_url'] = 'https://registry.example.com:5000'
 registry['secret_key'] = 'your_secret_key'
+registry_external_url 'https://registry.example.com:5000'
 
 gitlab_rails['gitlab_ci_cd'] = true
-gitlab_rails['gitlab_ci_cd_url'] = 'http://gitlab.example.com'
+gitlab_rails['gitlab_ci_cd_url'] = 'https://gitlab.example.com'
